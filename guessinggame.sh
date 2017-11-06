52fa4ed0411c8b514be507c357feca9afb460e49
Cortalak <akira.cortal@gmail.com>
2017-11-06 15:52:13 +0100

    finalmakefile

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,7 +1,10 @@
 echo "how many files are there in the directory"
+echo "Please enter your guess"
+function readvariable {
 read guess
+}
+readvariable
 nfile=$(ls -l | wc -l)
-echo "$nfile"
 while [ $guess -ne $nfile ]
 do
 	if [ $guess -gt $nfile ]
@@ -11,6 +14,7 @@ do
 			echo "too small"
 	fi
 	echo "guess again how many files are there in the directory"
-	read guess
+	readvariable
 done
 echo "Congratulation you guessed the right number of file"
+echo "Exiting Program..."
